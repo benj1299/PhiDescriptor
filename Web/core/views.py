1d1fe86f1d4673e43ec939a8b9d1dfa17038575c
Benjamin Kauffmann <benjaminkauffmann@hotmail.fr>
2021-12-12 20:30:07 +0100

    ajout du formulaire d'upload de fichiers sur django

--- Web/core/views.py
+++ Web/core/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from Descriptors.Helper import Helper
-import os
+from django.core.files.storage import FileSystemStorage
 
 def index(request):
     relation = None
-    if request.method == 'POST':
-        images = Helper.preprocessImages("./Descriptors/images")
+    if request.method == 'POST' and request.FILES['image']:
+        upload = request.FILES['image']
+        fss = FileSystemStorage()
+        file = fss.save(upload.name, upload)
+        file_url = fss.url(file)
+
+        images = Helper.preprocessImages("./media")
         objects = Helper.convertImagesToObjects(images)
         relation = objects[0].getRelationWith(objects[1])
         print(relation)
-        #image = request.POST.get("image")
-    
+
     context = {
         "relation": relation
     }
